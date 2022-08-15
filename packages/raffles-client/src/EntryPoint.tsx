d56a16546d9a5fa792fe624f3ce28085a61eeb1f
VetleFell <vetle.stubberud@gmail.com>
2022-08-15 12:20:02 +0100

    fix: user now updates in ranking

--- packages/raffles-client/src/EntryPoint.tsx
+++ packages/raffles-client/src/EntryPoint.tsx
@@ -55,7 +55,7 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
-const client = new ApolloClient({
+export const client = new ApolloClient({
   link: authLink.concat(splitLink),
   cache: new InMemoryCache(),
 });
